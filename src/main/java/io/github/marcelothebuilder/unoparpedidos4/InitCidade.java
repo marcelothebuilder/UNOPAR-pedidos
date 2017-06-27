79468909654a66a9176050d0dc3cebd62f7f699c
Marcelo Paixão Resende <marcelopaixaoresende@gmail.com>
2017-06-26 23:29:30 -0300

    remove obslete code

--- src/main/java/io/github/marcelothebuilder/unoparpedidos4/InitCidade.java
+++ src/main/java/io/github/marcelothebuilder/unoparpedidos4/InitCidade.java
@@ -1,25 +1,25 @@
-package io.github.marcelothebuilder.unoparpedidos4;
-
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-import io.github.marcelothebuilder.unoparpedidos4.service.DumbService;
-
-@Component
-@Transactional
-public class InitCidade {
-	@Autowired
-	private DumbService dumbService;
-
-	@EventListener(ContextRefreshedEvent.class)
-	@Transactional
-	private void onBoot(){
-		System.out.println(dumbService);
-		dumbService.onBoot();
-	}
-	
-}
+//package io.github.marcelothebuilder.unoparpedidos4;
+//
+//import javax.transaction.Transactional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//
+//import io.github.marcelothebuilder.unoparpedidos4.service.DumbService;
+//
+//@Component
+//@Transactional
+//public class InitCidade {
+//	@Autowired
+//	private DumbService dumbService;
+//
+//	@EventListener(ContextRefreshedEvent.class)
+//	@Transactional
+//	private void onBoot(){
+//		System.out.println(dumbService);
+//		dumbService.onBoot();
+//	}
+//	
+//}
